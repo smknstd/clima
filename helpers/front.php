df5533ef75bd07a83fa80c5f7d0b705684ee2299
Arnaud Becher <arnaud.becher@gmail.com>
2022-02-10 10:41:12 +0100

    add color for temps

--- helpers/front.php
+++ helpers/front.php
@@ -7,3 +7,21 @@ function format_report_value_from_storage($value, $precision = 2, $unit = null) 
     }
     return round(number_format($value/100, 2, ".", ""), $precision) . ($unit ? " " . $unit : "");
 }
+
+function get_bg_temperature($temperature)
+{
+    $roundedTemperature = (int)(ceil( round($temperature/100) / 5 ) * 5);
+
+    return match($roundedTemperature) {
+        -40, -35, -30, -25, -20, -15 => "bg-blue-800",
+        -10 => "bg-blue-600",
+        -5 => "bg-blue-400",
+        0 => "bg-blue-200",
+        5 => "bg-red-200",
+        10 => "bg-red-400",
+        15 => "bg-red-600",
+        20 => "bg-red-700",
+        25 => "bg-red-800",
+        30, 35, 40, 45, 50, 55 => "bg-red-900",
+    };
+}
--- resources/views/pages/reports.blade.php
+++ resources/views/pages/reports.blade.php
@@ -27,90 +27,90 @@
                         <div class="py-2 align-middle inline-block min-w-full sm: lg:px-8">
                             <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                                 <table class="min-w-full divide-y divide-gray-200">
-                                    <thead class="bg-gray-50">
+                                    <thead class="bg-gray-100">
                                     <tr>
                                         <th scope="col" class="pl-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Station</th>
-                                        <th scope="col" class=" py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Tn</th>
-                                        <th scope="col" class=" py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Tx</th>
-                                        <th scope="col" class=" py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">P</th>
-                                        <th scope="col" class=" py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Pn</th>
-                                        <th scope="col" class=" py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Px</th>
-                                        <th scope="col" class=" py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">RR</th>
-                                        <th scope="col" class=" py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">W</th>
-                                        <th scope="col" class=" py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nei</th>
-                                        <th scope="col" class=" py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Vd</th>
-                                        <th scope="col" class=" py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Vm</th>
-                                        <th scope="col" class=" py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Vx</th>
-                                        <th scope="col" class=" py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Observations</th>
+                                        <th scope="col" class="pl-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Tn</th>
+                                        <th scope="col" class="pl-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Tx</th>
+                                        <th scope="col" class="pl-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">P</th>
+                                        <th scope="col" class="pl-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Pn</th>
+                                        <th scope="col" class="pl-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Px</th>
+                                        <th scope="col" class="pl-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">RR</th>
+                                        <th scope="col" class="pl-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">W</th>
+                                        <th scope="col" class="pl-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">N</th>
+                                        <th scope="col" class="pl-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Vd</th>
+                                        <th scope="col" class="pl-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Vm</th>
+                                        <th scope="col" class="pl-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Vx</th>
+                                        <th scope="col" class="pl-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Observations</th>
                                     </tr>
                                     </thead>
                                     <tbody class="bg-white divide-y divide-gray-200">
                                     @foreach($reports as $report)
                                     <tr>
-                                        <td class="pl-3 py-4 whitespace-nowrap">
+                                        <td class="pl-3 py-4 whitespace-nowrap font-medium">
                                             {{ $report->weatherStation->city }}
                                         </td>
-                                        <td class="py-4 whitespace-nowrap">
+                                        <td class="pl-3 py-4 whitespace-nowrap font-medium {{ get_bg_temperature($report->min_temperature) }}">
                                             {{ format_report_value_from_storage($report->min_temperature, 1) }} °c
                                         </td>
-                                        <td class="py-4 whitespace-nowrap">
+                                        <td class="pl-3 py-4 whitespace-nowrap font-medium {{ get_bg_temperature($report->max_temperature) }}">
                                             {{ format_report_value_from_storage($report->max_temperature, 1) }} °c
                                         </td>
-                                        <td class="py-4 whitespace-nowrap">
+                                        <td class="pl-3 py-4 whitespace-nowrap bg-gray-100">
                                             {{ $report->pressure }}
                                         </td>
-                                        <td class="py-4 whitespace-nowrap">
+                                        <td class="pl-3 py-4 whitespace-nowrap">
                                             {{ $report->min_pressure }}
                                         </td>
-                                        <td class="py-4 whitespace-nowrap">
+                                        <td class="pl-3 py-4 whitespace-nowrap bg-gray-100">
                                             {{ $report->max_pressure }}
                                         </td>
-                                        <td class="py-4 whitespace-nowrap">
+                                        <td class="pl-3 py-4 whitespace-nowrap font-medium bg-green-100">
                                             {{ format_report_value_from_storage($report->precipitation,1, "cm") }}
                                         </td>
-                                        <td class="py-4 whitespace-nowrap">
+                                        <td class="pl-3 py-4 whitespace-nowrap bg-gray-100">
                                             {{ format_report_value_from_storage($report->sunshine_duration, 2, "h") }}
                                         </td>
-                                        <td class="py-4 whitespace-nowrap">
+                                        <td class="pl-3 py-4 whitespace-nowrap">
                                             {{ format_report_value_from_storage($report->snow_depth,1, "cm") }}
                                         </td>
-                                        <td class="py-4 whitespace-nowrap">
+                                        <td class="pl-3 py-4 whitespace-nowrap bg-gray-100">
                                             @if($report->wind_direction)
                                                 <span class="text-3xl">
                                                     <i class="wi wi-wind {{ $report->wind_direction->value }}"></i>
                                                 </span>
                                             @endif
                                         </td>
-                                        <td class="py-4 whitespace-nowrap">
+                                        <td class="pl-3 py-4 whitespace-nowrap">
                                             {{ format_report_value_from_storage($report->avg_wind_speed,1, "km/h") }}
                                         </td>
-                                        <td class="py-4 whitespace-nowrap">
+                                        <td class="pl-3 py-4 whitespace-nowrap bg-gray-100">
                                             {{ format_report_value_from_storage($report->max_wind_speed,1, "km/h") }}
                                         </td>
-                                        <td class="py-4 max-w-xs">
+                                        <td class="pl-3 py-4 max-w-xs">
                                             @if($report->has_rain)
                                                 <div class="px-3 py-2 inline-flex text-lg rounded-full bg-green-100 text-green-800"><i class="wi wi-raindrop"></i></div>
                                             @endif
                                             @if($report->has_storm)
                                                 <div class="px-3 py-2 inline-flex text-lg rounded-full bg-orange-100 text-orange-800"><i class="wi wi-lightning"></i></div>
                                             @endif
-                                                @if($report->has_hail)
-                                                    <div class="px-3 py-2 inline-flex text-xl rounded-full bg-blue-100 text-blue-800"><i class="wi wi-hail"></i></div>
-                                                @endif
-                                                @if($report->has_snow)
-                                                    <div class="px-3 py-2 inline-flex text-lg rounded-full bg-sky-100 text-sky-800"><i class="wi wi-snow"></i></div>
-                                                @endif
-                                                @if($report->has_fog)
-                                                    <div class="px-3 py-2 inline-flex text-lg rounded-full bg-yellow-100 text-yellow-800"><i class="wi wi-windy"></i></div>
-                                                @endif
-                                                @if($report->has_flood)
-                                                    <div class="px-3 py-2 inline-flex text-xl rounded-full bg-amber-100 text-amber-800"><i class="wi wi-flood"></i></div>
-                                                @endif
-                                                @if($report->comment)
-                                                    <div class="py-2 italic">
-                                                        {{ $report->comment }}
-                                                    </div>
-                                                @endif
+                                            @if($report->has_hail)
+                                                <div class="px-3 py-2 inline-flex text-xl rounded-full bg-blue-100 text-blue-800"><i class="wi wi-hail"></i></div>
+                                            @endif
+                                            @if($report->has_snow)
+                                                <div class="px-3 py-2 inline-flex text-lg rounded-full bg-sky-100 text-sky-800"><i class="wi wi-snow"></i></div>
+                                            @endif
+                                            @if($report->has_fog)
+                                                <div class="px-3 py-2 inline-flex text-lg rounded-full bg-yellow-100 text-yellow-800"><i class="wi wi-windy"></i></div>
+                                            @endif
+                                            @if($report->has_flood)
+                                                <div class="px-3 py-2 inline-flex text-xl rounded-full bg-amber-100 text-amber-800"><i class="wi wi-flood"></i></div>
+                                            @endif
+                                            @if($report->comment)
+                                                <div class="py-2 italic">
+                                                    {{ $report->comment }}
+                                                </div>
+                                            @endif
                                         </td>
                                     </tr>
                                     @endforeach
